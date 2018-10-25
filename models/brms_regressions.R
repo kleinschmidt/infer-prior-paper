ede4d7dd13e23a7ab1cb85abcb8c7ca520ecf37e
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2018-10-25 09:20:13 -0400

    updates on brms regression

--- models/brms_regressions.R
+++ models/brms_regressions.R
@@ -17,15 +17,41 @@ d <- supunsup::supunsup_clean %>%
          
 
 
-# this is _extremely slow. 
-b_logit <- brm(respP ~ 0 + bvotCond * supervised * vot_s * trial_s +
-                 (1 + vot_s | subject),
+# this is _extremely slow.
+f_noint <- brmsformula(respP ~ 0 + bvotCond * supervised * vot_s * trial_s +
+                         (1 + vot_s | subject),
+                       family=bernoulli())
+
+b_logit <- brm(f_noint,
                data = d,
                family = bernoulli(),
                chains=4,
                iter=1000)
 
 saveRDS(b_logit, "brm_logistic.rds")
+
+## this fits v. badly.  I suspect it might have something to do with the minus
+## ten condition but I'm really not sure.  the main effects of the conditions
+## are sensible except for the -10 one.
+##
+## I'm looking at the prior and it looks like there's no priors on the betas,
+## which leads to _crazy high_ (nonsense) values.  but maybe there's an issue
+## with how the priors are set in a non-intercept model?
+
+f_int = brmsformula(respP ~ 1 + bvotCond * supervised * vot_s * trial_s +
+                      (1 + vot_s | subject),
+                    family=bernoulli())
+
+get_prior(f_int, d)
+
+
+library(lme4)
+
+g_logit <- glmer(respP ~ 0 + bvotCond * supervised * vot_s * trial_s +
+                   (1 + vot_s | subject),
+                 data = d,
+                 family="bernoulli")
+
 
 
 b_logit_exp1 <- brm(respP ~ 0 + bvotCond*vot_s +
