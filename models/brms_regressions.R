67742963b4980ca8b0b34578234c5aba5cf795bd
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2018-10-30 15:48:21 -0400

    brms fit without -10 condition since that's not in supervised dat.

--- models/brms_regressions.R
+++ models/brms_regressions.R
@@ -4,12 +4,13 @@
 library(tidyverse)
 library(forcats)
 library(brms)
+library(tidybayes)
 options(mc.cores = parallel::detectCores())
 
 library(supunsup)
 
 d <- supunsup::supunsup_clean %>%
-  filter(labeled == "unlabeled") %>%
+  filter(labeled == "unlabeled", bvotCond != "-10") %>%
   select(subject, supCond, labeled, bvotCond, trial, vot, respP) %>%
   mutate(vot_s = (vot - mean(vot)) / sd(vot),
          trial_s = (trial - mean(trial)) / sd(trial),
@@ -18,9 +19,9 @@ d <- supunsup::supunsup_clean %>%
 
 
 # this is _extremely slow.
-f_noint <- brmsformula(respP ~ 0 + bvotCond * supervised * vot_s * trial_s +
-                         (1 + vot_s | subject),
-                       family=bernoulli())
+f_noint <- respP ~ 0 + bvotCond * supervised * vot_s * trial_s +
+  (1 + vot_s | subject)
+
 
 b_logit <- brm(f_noint,
                data = d,
@@ -30,6 +31,8 @@ b_logit <- brm(f_noint,
 
 saveRDS(b_logit, "brm_logistic.rds")
 
+b_logit <- readRDS("brm_logistic.rds")
+
 ## this fits v. badly.  I suspect it might have something to do with the minus
 ## ten condition but I'm really not sure.  the main effects of the conditions
 ## are sensible except for the -10 one.
@@ -37,25 +40,14 @@ saveRDS(b_logit, "brm_logistic.rds")
 ## I'm looking at the prior and it looks like there's no priors on the betas,
 ## which leads to _crazy high_ (nonsense) values.  but maybe there's an issue
 ## with how the priors are set in a non-intercept model?
-
-f_int = brmsformula(respP ~ 1 + bvotCond * supervised * vot_s * trial_s +
-                      (1 + vot_s | subject),
-                    family=bernoulli())
-
-get_prior(f_int, d)
-
+##
+## oh FUCK the issue that there's no bvotCond=-10 in the supervised group....
+## once that's taken care of, all good.
 
 library(lme4)
 
-g_logit <- glmer(respP ~ 0 + bvotCond * supervised * vot_s * trial_s +
-                   (1 + vot_s | subject),
+# doesn't converge so fuck it
+g_logit <- glmer(f_noint,
                  data = d,
-                 family="bernoulli")
+                 family = "binomial")
 
-
-
-b_logit_exp1 <- brm(respP ~ 0 + bvotCond*vot_s +
-                      (1 + vot_s ~ subject),
-                    data = filter(d, supCond == "unsupervised"),
-                    family = bernoulli(),
-                    chains=8, iter=1000)
