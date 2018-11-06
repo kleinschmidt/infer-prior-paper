4f6eb77a2df4d142b27db82c97f546d42875cf95
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2018-11-06 16:09:57 -0500

    clean up brms, extract curves, plot, update TODOs

--- models/brms_regressions.R
+++ models/brms_regressions.R
@@ -9,45 +9,104 @@ options(mc.cores = parallel::detectCores())
 
 library(supunsup)
 
+################################################################################
+# expt 1
+
+d1 <- supunsup::supunsup_clean %>%
+  filter(supCond == "unsupervised") %>%
+  select(subject, bvotCond, trial, vot, respP) %>%
+  mutate(vot_s = (vot - mean(vot)) / sd(vot),
+         trial_s = (trial - mean(trial)) / sd(trial))
+
+f <- respP ~ 1 + bvotCond * vot_s * trial_s + (1 + vot_s | subject)
+b_logit_exp1 <- brm(f, data=d1, family=bernoulli(), chains=4, iter=1000)
+
+saveRDS(b_logit_exp1, "brm_logistic_exp1.rds")
+b_logit_exp1 <- readRDS("brm_logistic_exp1.rds")
+
+## extract boundaries
+
+fits <- fitted(b_logit_exp1) %>% as.tibble() %>% bind_cols(d1)
+
+fits_avg <- fitted(b_logit_exp1, re_formula = NA) %>% as.tibble() %>% bind_cols(d1)
+
+ggplot(fits, aes(x=vot_s, y=Estimate, group=subject)) +
+  geom_line(stat="summary", fun.y=mean) +
+  facet_grid(ntile(trial, 6) ~ bvotCond)
+
+d1_blocks <-
+  d1 %>%
+  group_by(block=ntile(trial, 3)) %>%
+  summarise_at(vars(trial, trial_s), mean)
+
+data_pred <-
+  cross_df(list(vot_s = seq(min(d1$vot_s), max(d1$vot_s), length.out=100),
+                bvotCond = unique(d1$bvotCond),
+                block = 1:3)) %>%
+  left_join(d1_blocks)
+
+data_pred_subj <-
+  d1 %>%
+  group_by(subject, bvotCond) %>%
+  summarise() %>%
+  left_join(data_pred, by="bvotCond")
+
+expt1_bounds <-
+  fitted(b_logit_exp1, newdata=data_pred, re_formula=NA) %>%
+  as_tibble() %>%
+  bind_cols(data_pred)
+
+expt1_bounds_bysub <-
+  fitted(b_logit_exp1, newdata=data_pred_subj) %>%
+  as_tibble() %>%
+  bind_cols(data_pred_subj)
+  
+  
+ggplot(expt1_bounds,
+       aes(x=vot_s, y=Estimate, ymin=`Q2.5`, ymax=`Q97.5`,
+           color=bvotCond, fill=bvotCond)) +
+  geom_ribbon(alpha=0.1, color=NA) +
+  geom_line() +
+  facet_grid(.~trial)
+
+
+ggplot(mapping=aes(x=vot_s, y=Estimate, color=bvotCond, fill=bvotCond)) +
+  geom_line(data=expt1_bounds_bysub %>% filter(block==3),
+            aes(group=subject), alpha=0.2) +
+  facet_grid(.~bvotCond)
+
+expt1_bounds %>%
+  filter(block == 3) %>%
+  ggplot(aes(x=vot_s, y=Estimate, color=bvotCond, fill=bvotCond)) +
+  geom_line(data=expt1_bounds_bysub %>% filter(block==3),
+            aes(group=subject), alpha=0.2) +
+  geom_ribbon(aes(fill=bvotCond, ymin=`Q2.5`, ymax=`Q97.5`), alpha=0, linetype=2) +
+  geom_line(size=2) +
+  facet_grid(.~bvotCond)
+
+
+################################################################################
+# expt 1 vs 2: effect of supervision
+
 d <- supunsup::supunsup_clean %>%
   filter(labeled == "unlabeled", bvotCond != "-10") %>%
   select(subject, supCond, labeled, bvotCond, trial, vot, respP) %>%
   mutate(vot_s = (vot - mean(vot)) / sd(vot),
          trial_s = (trial - mean(trial)) / sd(trial),
          supervised = fct_recode(supCond, supervised="mixed"))
-         
 
-
-# this is _extremely slow.
 f_noint <- respP ~ 0 + bvotCond * supervised * vot_s * trial_s +
   (1 + vot_s | subject)
 
-
-b_logit <- brm(f_noint,
+b_logit_sup_v_unsup <- brm(f_noint,
                data = d,
                family = bernoulli(),
                chains=4,
                iter=1000)
 
-saveRDS(b_logit, "brm_logistic.rds")
+saveRDS(b_logit_sup_v_unsup, "brm_logistic_sup_v_unsup.rds")
 
-b_logit <- readRDS("brm_logistic.rds")
+b_logit_sup_v_unsup <- readRDS("brm_logistic_sup_v_unsup.rds")
 
-## this fits v. badly.  I suspect it might have something to do with the minus
-## ten condition but I'm really not sure.  the main effects of the conditions
-## are sensible except for the -10 one.
-##
-## I'm looking at the prior and it looks like there's no priors on the betas,
-## which leads to _crazy high_ (nonsense) values.  but maybe there's an issue
-## with how the priors are set in a non-intercept model?
-##
-## oh FUCK the issue that there's no bvotCond=-10 in the supervised group....
-## once that's taken care of, all good.
 
-library(lme4)
-
-# doesn't converge so fuck it
-g_logit <- glmer(f_noint,
-                 data = d,
-                 family = "binomial")
 
