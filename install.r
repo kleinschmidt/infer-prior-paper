e7157022a98c76f58ea974b230151f03162900d2
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2020-05-19 17:55:33 -0400

    map port 2222 to 22, actually install dependencies

--- install.r
+++ install.r
@@ -1,32 +1,28 @@
 # set up dependencies in docker
 
-
-
-## deps <- c(
-##   "rmarkdown",
-##   "knitr",
-##   "tidyverse",
-##   "dplyr",
-##   "glue",
-##   "forcats",
-##   "magrittr",
-##   "purrr",
-##   "purrrlyr",
-##   "stringr",
-##   "beliefupdatr",
-##   "supunsup",
-##   "rstan",
-##   "brms",
-##   "tidybayes",
-##   "cowplot",
-##   "ggbeeswarm",
-##   "latex2exp",
-##   "kleinschmidt/daver",
-##   "kleinschmidt/phonetic-sup-unsup",
-##   "kleinschmidt/beliefupdatr"
-## )
-
-deps <- c("rmarkdown", "kleinschmidt/votcorpora")
+deps <- c(
+  "rmarkdown",
+  "knitr",
+  "tidyverse",
+  "dplyr",
+  "glue",
+  "forcats",
+  "magrittr",
+  "purrr",
+  "purrrlyr",
+  "stringr",
+  "beliefupdatr",
+  "supunsup",
+  "rstan",
+  "brms",
+  "tidybayes",
+  "cowplot",
+  "ggbeeswarm",
+  "latex2exp",
+  "kleinschmidt/daver",
+  "kleinschmidt/phonetic-sup-unsup",
+  "kleinschmidt/beliefupdatr"
+)
 
 renv::install(deps, prompt=FALSE)
 renv::snapshot(type="all")
